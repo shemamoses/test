6c443cca50ded216d709152accf92dcdededb1df
mosesshema1@gmail.com <mosesshema1@gmail.com>
2023-12-02 12:02:53 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Header from "./Header"
   return (
     <div>
       <Header/>
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
     </div>
   )
 }
