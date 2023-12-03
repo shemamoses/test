43f76d47873b8e37aa7443a6d97f2e6bcd80b850
mosesshema1@gmail.com <mosesshema1@gmail.com>
2023-12-03 13:58:30 +0200

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import Header from "./Header"
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
 
- function App() {
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div>
-      <Header/>
+      <Header />
 
       <main className="main">
         <p>1/15</p>
         <p>Question?</p>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Mains.jsx
@@ -0,0 +1,9 @@
+import { Children } from "react"
+
+function Mains(){
+    return<Mains className="main">
+        {Children}
+    </Mains>
+}
+
+export default Mains
